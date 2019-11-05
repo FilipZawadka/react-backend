48f64d1dd59e172feaddcd5f6f38f69b6056e8a0
FilipZawadka <38583399+FilipZawadka@users.noreply.github.com>
2019-11-05 23:46:05 +0100

    lab4task3

--- src/main/java/pw/react/backend/reactbackend/controllers/UserController.java
+++ src/main/java/pw/react/backend/reactbackend/controllers/UserController.java
@@ -1,7 +1,12 @@
 package pw.react.backend.reactbackend.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pw.react.backend.reactbackend.models.User;
 import pw.react.backend.reactbackend.repositories.UserRepository;
 
@@ -25,6 +30,27 @@ public class UserController {
     @PostMapping
     public User create(@RequestBody final User user){
         return userRepository.saveAndFlush(user);
+    }
+
+    @RequestMapping(value="{id}",method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        userRepository.deleteById(id);
+    }
+
+
+    @RequestMapping(value="{id}",method = RequestMethod.PUT)
+    public User update(@PathVariable Long id, @RequestBody User user){
+        User existingUser = userRepository.getOne(id);
+        BeanUtils.copyProperties(user,existingUser,"id");
+        return userRepository.saveAndFlush(existingUser);
+    }
+
+    @Configuration
+    public class ResourceConfig extends WebMvcConfigurerAdapter {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/images/**").addResourceLocations("file:images/");
+        }
     }
 }
 
